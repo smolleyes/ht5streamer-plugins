2fa9f24e2bc1f23703cb4b74d686c26d93c2e08e
Smolleyes <s.lagui@free.fr>
2014-04-27 01:24:37 +0200

    fix

--- mega-search/main.js
+++ mega-search/main.js
@@ -25,6 +25,7 @@ var scannedLinks = 0;
 var totalFiles = 0;
 var folderList = [];
 var linksList = [];
+var maCleDeCryptage = "Sr95tYU1";
 
 megaSearch.init = function(gui,ht5,notif) {
   megaSearch.mainWin = gui;
@@ -261,13 +262,10 @@ function loadPageLinks(list,item,totalLinks) {
             console.log("titre inconnu " + link)
             var titre = "titre iconnu...";
           }
-          var megaLink = $('area',res)[0].href;
-          if (megaLink.indexOf('download.php') !== -1) {
-              var mid = megaLink.match(/(.*)download.php(.*)/)[2];
-              megaLink = 'http://curl.mega-search.ws/download.php'+mid;
-          }
-          console.log(megaLink)
-          if (titre === '') {
+          var code = res.match(/id="code"(.*?)>(.*?)<\/div>/)[2];
+          var megaLink = decrypter(maCleDeCryptage,code);
+          console.log(titre)
+          if ((titre === '') || (titre == undefined)) {
             titre = item.title;
           } 
           if (megaLink.match(/https:\/\/mega.co.nz\/#F!/) !== null) {
@@ -1065,6 +1063,95 @@ function from256to128(s) {
   }
   return o
 }
+
+function decrypter(maCleDeCryptage, maChaineCrypter){
+  maCleDeCryptage = node_crypto.createHash('md5').update(maCleDeCryptage).digest("hex");
+  letter = -1;
+  newstr = '';
+  maChaineCrypter = base64_decode(maChaineCrypter);
+  strlen = maChaineCrypter.length;
+  for ( i = 0; i < strlen; i++ ) {
+    letter+=1;
+    if ( letter > 31 ){
+      letter = 0;
+    }
+    neword = ord(maChaineCrypter[i]) - ord(maCleDeCryptage[letter]);
+    if ( neword < 1 ){
+      neword += 256;
+    }
+    //console.log(neword)
+    newstr += chr(neword);
+    
+    if(i+1 == strlen) {
+      return newstr;
+    }
+  }
+}
+
+
+function chr(n) {
+    return String.fromCharCode(n);
+}
+
+function ord(ch) {
+    //console.log("ask: "+ch +" return: " + ch.charCodeAt(0))
+    return ch.charCodeAt(0);
+}
+
+function base64_decode(data) {
+  //  discuss at: http://phpjs.org/functions/base64_decode/
+  // original by: Tyler Akins (http://rumkin.com)
+  // improved by: Thunder.m
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //    input by: Aman Gupta
+  //    input by: Brett Zamir (http://brett-zamir.me)
+  // bugfixed by: Onno Marsman
+  // bugfixed by: Pellentesque Malesuada
+  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //   example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
+  //   returns 1: 'Kevin van Zonneveld'
+  //   example 2: base64_decode('YQ===');
+  //   returns 2: 'a'
+
+  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    dec = '',
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+  data += '';
+
+  do { // unpack four hexets into three octets using index points in b64
+    h1 = b64.indexOf(data.charAt(i++));
+    h2 = b64.indexOf(data.charAt(i++));
+    h3 = b64.indexOf(data.charAt(i++));
+    h4 = b64.indexOf(data.charAt(i++));
+
+    bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+
+    o1 = bits >> 16 & 0xff;
+    o2 = bits >> 8 & 0xff;
+    o3 = bits & 0xff;
+
+    if (h3 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1);
+    } else if (h4 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2);
+    } else {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+    }
+  } while (i < data.length);
+
+  dec = tmp_arr.join('');
+
+  return dec.replace(/\0+$/, '');
+}
+
 
 
 module.exports = megaSearch;
